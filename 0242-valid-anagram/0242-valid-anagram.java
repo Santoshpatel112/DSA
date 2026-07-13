0150f0451ebbbd1066ab061178d64068175df290
Santosh Patel <134463761+Santoshpatel112@users.noreply.github.com>
2026-07-13 12:20:38 +0530

    Time: 18 ms (13.58%), Space: 46.8 MB (5.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
+        if(s.length() !=t.length()){
             return false;
         }
-
         HashMap<Character,Integer> map=new HashMap<>();
-
         for(char ch:s.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
 
-        for(char ch:t.toCharArray()){
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+
             if(!map.containsKey(ch)){
                 return false;
             }
+
             map.put(ch,map.get(ch)-1);
-            if(map.get(ch) <0){
-                return false;
+
+            if(map.get(ch)==0){
+                map.remove(ch);
             }
         }
-        return true;
+        return map.isEmpty();
     }
 }
