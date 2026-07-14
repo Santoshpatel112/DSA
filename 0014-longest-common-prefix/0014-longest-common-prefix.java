fcfac1e305a3b7853ea3133e68c7e9edde61a02b
Santosh Patel <134463761+Santoshpatel112@users.noreply.github.com>
2026-07-14 08:47:18 +0530

    Time: 1 ms (62.7%), Space: 43.5 MB (17.32%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -7,12 +7,15 @@ class Solution {
             if(pre.charAt(i)==current.charAt(i)){
                 sb.append(pre.charAt(i));
             }
+            else{
+                break;
+            }
         }
         return sb.toString();
     }
     public String longestCommonPrefix(String[] strs) {
         int n=strs.length;
-
+        if(n==0)return "";
         String prefix=strs[0];
 
         for(int i=0;i<n;i++){
