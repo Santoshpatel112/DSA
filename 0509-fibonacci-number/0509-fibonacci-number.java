cb02436d649763c626c8fe73eb3dbb4bbc5b95c8
Santosh Patel <134463761+Santoshpatel112@users.noreply.github.com>
2026-07-28 03:15:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.64%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,15 @@
 class Solution {
-    public static int Fibo(int n,int dp[]){
+    public int fib(int n) {
         if(n<=1){
             return n;
         }
-        if(dp[n] !=-1){
-            return dp[n];
-        }
-
-        return dp[n]=Fibo(n-1,dp)+Fibo(n-2,dp);
-    }
-    public int fib(int n) {
         int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return Fibo(n,dp);
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
